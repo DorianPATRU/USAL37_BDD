5a227c392c89a18b14a2cbb1bf0531a77ab88799
DPatru <73827969+DorianPATRU@users.noreply.github.com>
2020-12-10 14:23:55 +0100

    maj

--- TP_Agencevoyages/TP_AgenceVoyage_1_clients.sql
+++ TP_Agencevoyages/TP_AgenceVoyage_1_clients.sql
@@ -5,51 +5,47 @@
 	Chaque client est associé à un commercial différent 
     
 3) 	Sélectionnez tous les clients (nom du commercial associé inclus)
+
 */
 
 use usal37_agence;
 
-CREATE TABLE sales
+CREATE TABLE sales 
 (
-   com_code VARCHAR(5),
-   com_name VARCHAR(64) NOT NULL,
-   com_password VARCHAR(60) NOT NULL,
-   PRIMARY KEY (com_code)
+	com_code CHAR(5) PRIMARY KEY,
+    com_name VARCHAR(64),
+    com_password CHAR(60)
 );
 
-INSERT INTO sales
-(com_code, com_name, com_password)
-VALUES
-('1', 'Frank', '******'),
-('2', 'Steven', '******'),
-('3', 'James', '******'),
-('4', 'Alain', '******'),
-('5', 'Richard', '******');
+INSERT INTO sales 
+(com_code, com_name, com_password) 
+VALUES 
+('BA201', 'Berthier Aline', '$2y$10$OpUTjAUsVuKtCstwAq5DeOVWPgofb2d2v.tsQuUCIgezmBqiv4fEi'),
+('NJ247', 'Neymar Jean', '$2y$10$OpUTjAUsVuKtCstwAq5DeOVWPgofb2d2v.tsQuUCIgezmBqiv4fEi'),
+('PJ714', 'Paute Jessie', '$2y$10$OpUTjAUsVuKtCstwAq5DeOVWPgofb2d2v.tsQuUCIgezmBqiv4fEi'),
+('PM654', 'Poglio Marcel', '$2y$10$OpUTjAUsVuKtCstwAq5DeOVWPgofb2d2v.tsQuUCIgezmBqiv4fEi'),
+('YT023', 'Yoyo Tata', '$2y$10$OpUTjAUsVuKtCstwAq5DeOVWPgofb2d2v.tsQuUCIgezmBqiv4fEi');
 
-CREATE TABLE clients
+
+CREATE table clients 
 (
-   client_id INT AUTO_INCREMENT,
-   client_lastname VARCHAR(32) NOT NULL,
-   client_firstname VARCHAR(32) NOT NULL,
-   client_email VARCHAR(128) NOT NULL,
-   client_phone VARCHAR(16) NOT NULL,
-   client_added DATE NOT NULL,
-   client_password VARCHAR(60) NOT NULL,
-   com_code VARCHAR(5) NOT NULL,
-   PRIMARY KEY (client_id),
-   FOREIGN KEY (com_code) REFERENCES sales (com_code)
+	client_id INT PRIMARY KEY AUTO_INCREMENT,
+    client_lastname VARCHAR(32),
+    client_firstname VARCHAR(32),
+    client_email VARCHAR(128),
+    client_phone CHAR(16),
+    client_added DATE,
+    client_password CHAR(60)
 );
 
-INSERT INTO clients
-(client_id, client_lastname, client_firstname, client_email, client_phone, client_added, client_password, com_code)
-VALUES
-('2496', 'DUPOND', 'Martin', 'dupond.martin@hotmail.com', '+336 45418978', '2011-08-20', '*****', '2'),
-('1028', 'FISCHER', 'Thierry', 'fischer.thierry@gmail.com', '+336 26119635', '2006-07-11', '*******', '4'),
-('9968', 'MEYER', 'Jacques', 'meyer.jacques@orange.fr', '+336 23488936', '2009-10-09', '******', '1'),
-('5467', 'NUSSBAUM', 'Eric', 'nussbaum.eric@yahoo.com', '+337 14742369', '2015-05-19', '*****', '5'),
-('1489', 'DEMANGES', 'Julien', 'demanges.julien@outlook.fr', '+337 16781598', '2019-01-24', '*******', '3');
-
-SELECT * FROM clients
-JOIN sales ON clients.com_code = sales.com_code
+INSERT INTO clients 
+(client_lastname, client_firstname, client_email, client_phone, client_added, client_password) 
+VALUES 
+('Dupont', 'Ernest', 'a@a.fr', 		'0102030405', NOW(), '$2y$10$OpUTjAUsVuKtCstwAq5DeOVWPgofb2d2v.tsQuUCIgezmBqiv4fEi'), 
+('Dupond', 'Louis', 'b@b.fr', 		'0203040506', NOW(), '$2y$10$OpUTjAUsVuKtCstwAq5DeOVWPgofb2d2v.tsQuUCIgezmBqiv4fEi'), 
+('Martin', 'Léo', 'c@c.fr', 	 	'0312345678', NOW(), '$2y$10$OpUTjAUsVuKtCstwAq5DeOVWPgofb2d2v.tsQuUCIgezmBqiv4fEi'), 
+('Devoldère', 'Mickaël', 'd@d.fr', 	'0678963214', NOW(), '$2y$10$OpUTjAUsVuKtCstwAq5DeOVWPgofb2d2v.tsQuUCIgezmBqiv4fEi'), 
+('Ben', 'Joe', 'e@e.fr', 			'0698741235', NOW(), '$2y$10$OpUTjAUsVuKtCstwAq5DeOVWPgofb2d2v.tsQuUCIgezmBqiv4fEi');
 
 
+SELECT * FROM clients;
