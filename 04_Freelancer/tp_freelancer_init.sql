b5086207a1caaf64cc3ee88c08575f564fb326bf
DPatru <73827969+DorianPATRU@users.noreply.github.com>
2021-01-07 18:41:03 +0100

    update

--- 04_Freelancer/tp_freelancer_init.sql
+++ 04_Freelancer/tp_freelancer_init.sql
@@ -6,7 +6,7 @@ USE tp_freelancer;
 
 CREATE TABLE customers 
 (
-    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+	customer_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_name VARCHAR(100) NOT NULL,
     customer_email VARCHAR(255),
     cat_id INT NOT NULL
@@ -14,39 +14,42 @@ CREATE TABLE customers
 
 CREATE TABLE customers_cats
 (
-    cat_id INT PRIMARY KEY AUTO_INCREMENT,
+	cat_id INT PRIMARY KEY AUTO_INCREMENT,
     cat_name VARCHAR(50) NOT NULL,
     cat_description TEXT NULL
 );
 
-CREATE TABLE quotes 
-(
-    quotes_id INT PRIMARY KEY AUTO_INCREMENT,
-    quote_state CHAR(10) NOT NULL,
-    quote_date DATE NOT NULL,
-    quote_amount INT NOT NULL,
-    quote_final_date DATE,
-    quote_final_amount INT 
-);
-
 CREATE TABLE jobs
 (
-    job_id INT PRIMARY KEY AUTO_INCREMENT,
+	job_id INT PRIMARY KEY AUTO_INCREMENT,
     job_state CHAR(10) NOT NULL,
     job_title VARCHAR(100) NOT NULL,
-    job_start DATETIME,
-    job_end DATETIME,
-    job_description TEXT
+    job_start DATETIME NULL,
+    job_end DATETIME NULL,
+    job_description TEXT NULL,
+    customer_id INT NOT NULL
 );
+
+CREATE TABLE quotes 
+(
+	quote_id INT PRIMARY KEY AUTO_INCREMENT,
+    quote_state CHAR(10) NOT NULL,
+    quote_date DATE NOT NULL,
+    quote_amount INT,
+    quote_final_date DATE NULL,
+    quote_final_amount INT NULL,
+    job_id INT NOT NULL
+);
+
 
 ALTER TABLE customers 
 ADD 
-    CONSTRAINT fk_customerscat FOREIGN KEY (cat_id) REFERENCES customers_cats (cat_id);
-/*
-ALTER TABLE jobs
-ADD 
-    CONSTRAINT fk_jobsquote FOREIGN KEY (quote_id) REFERENCES quotes(quote_id);
+	CONSTRAINT fk_customercat FOREIGN KEY (cat_id) REFERENCES customers_cats(cat_id);
 
-ALTER TABLE customers
+ALTER TABLE jobs
 ADD
-        CONSTRAINT fk_customers_jobs FOREIGN KEY (job_id) REFERENCES jobs (job_id);
+	CONSTRAINT fk_customers_jobs FOREIGN KEY (customer_id) REFERENCES customers (customer_id);
+
+ALTER TABLE quotes
+ADD 
+	CONSTRAINT fk_jobquote FOREIGN KEY (job_id) REFERENCES jobs(job_id);
--- 04_Freelancer/tp_freelancer_selects.sql
+++ 04_Freelancer/tp_freelancer_selects.sql
@@ -2,23 +2,57 @@ use tp_freelancer;
 
 SELECT cat_name FROM customers_cats;
 
-SELECT customer_name, cat_name
-FROM customers
+SELECT customer_name, cat_name 
+FROM customers 
+JOIN customers_cats ON customers.cat_id = customers_cats.cat_id;
+
+/* affiche les titres des missions avec pour chaque mission 
+	le nom du client et le nom de catégorie du client */
+
+SELECT job_title, customer_name, cat_name 
+FROM jobs 
+JOIN customers ON jobs.customer_id = customers.customer_id 
 JOIN customers_cats ON customers.cat_id = customers_cats.cat_id;
 
 
-SELECT customer_id, customer_name, customer_email
-FROM customers;
+/* affiche les titres des missions avec pour chaque mission 
+	l'id et le nom du client et le nom de catégorie du client */
+
+SELECT 
+jobs.job_title, 
+jobs.customer_id, 
+customers.customer_name, 
+customers_cats.cat_name 
+FROM jobs 
+JOIN customers ON jobs.customer_id = customers.customer_id 
+JOIN customers_cats ON customers.cat_id = customers_cats.cat_id; 
 
 
--- affiche les titres des missions avec pour chaque mission
--- l'id et le nom du client et le nom de catégorie du client
+
+/* affiche les titres des missions avec pour chaque mission 
+	l'id et le nom du client et le nom de catégorie du client */
 
 SELECT 
 job_title AS 'Titre de la mission', 
-J.customer_id,
+jobs.customer_id, 
 customer_name, 
-cat_name
-FROM jobs AS J
-JOIN customers AS C ON jobs.customer_id = C.customer_id
-JOIN customers_cats AS CAT ON C.cat_id = CAT.cat_id;
+cat_name 
+FROM jobs 
+JOIN customers ON jobs.customer_id = customers.customer_id 
+JOIN customers_cats ON customers.cat_id = customers_cats.cat_id; 
+
+
+
+/* affiche les titres des missions avec pour chaque mission 
+	l'id et le nom du client et le nom de catégorie du client */
+
+SELECT 
+job_title AS 'Titre de la mission', 
+J.customer_id, 
+customer_name, 
+cat_name 
+FROM jobs AS J 
+JOIN customers AS C ON J.customer_id = C.customer_id 
+JOIN customers_cats AS CAT ON C.cat_id = CAT.cat_id; 
+
+
